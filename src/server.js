07326bb13b651bd0385261b1a7119e7f7f5a88ab
Kuldeep <kuldeeep.yadav1@gmail.com>
2026-05-04 22:48:50 +0530

    Add docker support

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
+import "dotenv/config";
 import { app } from "./app.js";
 
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
   console.log(`Server listening on PORT:${PORT}`);
